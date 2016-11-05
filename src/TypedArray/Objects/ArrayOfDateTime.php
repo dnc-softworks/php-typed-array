3e315cb58debd51289cb0a129ff599dd422de05b
Douglas Gubert <douglas.gubert@gmail.com>
2016-11-05 00:20:04 -0200

    ArrayOfDateTime as example of ArrayOfObject implementation

---
+++ src/TypedArray/Objects/ArrayOfDateTime.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: douglas
+ * Date: 11/5/16
+ * Time: 12:15 AM
+ */
+
+namespace dncsoftworks\TypedArray\Objects;
+
+use dncsoftworks\TypedArray\ArrayOfObject;
+
+class ArrayOfDateTime extends ArrayOfObject
+{
+
+}
